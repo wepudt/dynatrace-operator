75968c003b3f77843a9c3769200987748ca2d7d8
wepudt <werner.putschoegl@dynatrace.com>
2023-05-25 17:14:37 +0200

    casing small

--- hack/make/prerequisites.mk
+++ hack/make/prerequisites.mk
@@ -1,13 +1,13 @@
 #renovate depName=sigs.k8s.io/kustomize/kustomize/v5
-KUSTOMIZE_VERSION=v5.0.0
+kustomize_version=v5.0.0
 #renovate depName=sigs.k8s.io/controller-tools/cmd
-CONTROLLER_GEN_VERSION=v0.10.0
+controller_gen_version=v0.10.0
 # renovate depName=github.com/golangci/golangci-lint
-GOLANG_CI_CMD_VERSION=v1.52.0
+golang_ci_cmd_version=v1.52.0
 # renovate depName=github.com/daixiang0/gci
-GCI_VERSION=v0.10.0
+gci_version=v0.10.0
 # renovate depName=golang.org/x/tools
-GOLANG_TOOLS_VERSION=v0.9.0
+golang_tools_version=v0.9.0
 
 # Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
 ifeq (,$(shell go env GOBIN))
@@ -18,18 +18,18 @@ endif
 
 ## Installs 'kustomize' if it is missing
 prerequisites/kustomize:
-	hack/build/command.sh kustomize "sigs.k8s.io/kustomize/kustomize/v5@$(KUSTOMIZE_VERSION)"
+	hack/build/command.sh kustomize "sigs.k8s.io/kustomize/kustomize/v5@$(kustomize_version)"
 KUSTOMIZE=$(shell hack/build/command.sh kustomize)
 
 ## Install 'controller-gen' if it is missing
 prerequisites/controller-gen:
-	hack/build/command.sh controller-gen "sigs.k8s.io/controller-tools/cmd/controller-gen@$(CONTROLLER_GEN_VERSION)"
+	hack/build/command.sh controller-gen "sigs.k8s.io/controller-tools/cmd/controller-gen@$(controller_gen_version)"
 CONTROLLER_GEN=$(shell hack/build/command.sh controller-gen)
 
 prerequisites/setup-pre-commit:
-	go install github.com/golangci/golangci-lint/cmd/golangci-lint@$(GOLANG_CI_CMD_VERSION)
-	go install github.com/daixiang0/gci@$(GCI_VERSION)
-	go install golang.org/x/tools/cmd/goimports@$(GOLANG_TOOLS_VERSION)
+	go install github.com/golangci/golangci-lint/cmd/golangci-lint@$(golang_ci_cmd_version)
+	go install github.com/daixiang0/gci@v$(gci_version)
+	go install golang.org/x/tools/cmd/goimports@$(golang_tools_version)
 	cp ./.github/pre-commit ./.git/hooks/pre-commit
 	chmod +x ./.git/hooks/pre-commit
 
